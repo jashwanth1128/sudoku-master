f634dbb9a165b7028e05703756944f8a8f4433e1
Jashwanth Chitla <jashwanthmudiraj5@gmail.com>
2026-02-22 14:20:41 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,23 @@
+const CACHE_NAME = "sudoku-cache-v1";
+
+const urlsToCache = [
+  "/",
+  "/index.html",
+  "/styles.css",
+  "/script.js",
+  "/manifest.json"
+];
+
 self.addEventListener("install", event => {
-  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
 });
 
 self.addEventListener("fetch", event => {
-  event.respondWith(fetch(event.request));
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
